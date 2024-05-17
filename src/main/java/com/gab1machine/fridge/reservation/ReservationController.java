82f5f82545c2fca64691d038b05727132ed7c143
Ferreira-Orlann <118378969+Sora-u@users.noreply.github.com>
2024-05-17 16:45:54 +0200

    ReservationController => Restful

--- src/main/java/com/gab1machine/fridge/reservation/ReservationController.java
+++ src/main/java/com/gab1machine/fridge/reservation/ReservationController.java
@@ -2,6 +2,7 @@ package com.gab1machine.fridge.reservation;
 
 import com.gab1machine.fridge.storage.StorageDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,28 +12,38 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-@RequestMapping("/reservation")
+@RequestMapping("/reservations")
 @RestController
 public class ReservationController {
     private final ReservationServices reservationServices;
 
-    @Operation(summary = "Get a reservation by id")
+    @Operation(summary = "Get all reservations")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Retreived successfully",
                     content = {
                             @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ReservationDto.class)
+                                    array = @ArraySchema(schema = @Schema(implementation = ReservationDto.class))
                             )
                     }),
     })
     @GetMapping
-    public @ResponseBody ResponseEntity<ReservationOutputDto> getReservation(@PathVariable @RequestParam(name = "id", required = true) UUID id) {
-        Optional<ReservationDto> dto = this.reservationServices.getReservation(id);
-        return dto.map(reservationDto -> ResponseEntity.ok(this.reservationServices.dtoToODto(reservationDto))).orElseGet(() -> ResponseEntity.badRequest().build());
+    public @ResponseBody ResponseEntity<List<ReservationDto>> getReservations() {
+        return ResponseEntity.ok(this.reservationServices.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody ResponseEntity<ReservationOutputDto> getReservation(@PathVariable @RequestParam(name = "id", required = false) UUID id) {
+        if (id != null) {
+            Optional<ReservationDto> dto = this.reservationServices.getReservation(id);
+            return dto.map(reservationDto -> ResponseEntity.ok(this.reservationServices.dtoToODto(reservationDto))).orElseGet(() -> ResponseEntity.badRequest().build());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Operation(summary = "Create a Reservation")
